efa4b2b4faf5c316d265ecbb899b9fa3e7b9d6a2
Anxiety <aniketpromys@gmail.com>
2025-11-22 22:01:27 +0530

    nn

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aniketink.github.io',
+  base: '/anxiety_porfolio',
 
   vite: {
     plugins: [tailwindcss()]
